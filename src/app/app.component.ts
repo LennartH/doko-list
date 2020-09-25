e11c9f6db979c34ec2aa35933b4eaf90da3946d1
Lennart Hensler <LennartHensler@gmail.com>
2020-09-25 14:38:12 +0200

    Add listener to prevent browser context menu on long press

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
@@ -17,6 +17,15 @@ export class AppComponent {
   ) {
     this.initializeApp();
   }
+
+  @HostListener("contextmenu", ['$event'])
+  preventContextMenuGlobally(event: MouseEvent) {
+    // Right click events on mobile have button 0 but coordinates (1, 1)
+    if (event.button === 0 && event.screenX !== 1 && event.screenY !== 1) {
+      event.preventDefault();
+    }
+  }
+
 
   initializeApp() {
     this.platform.ready().then(() => {
